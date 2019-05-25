c462016a947c1afc2c5288818d4058692696b3a8
Nathan Cooper <nac33@students.uwf.edu>
2019-05-25 00:21:02 -0400

    Add model weights, tokenizer, and demo predictor for example image

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from PIL import Image
+import recipe
+
 app = Flask(__name__)
+recipe_namer = recipe.get_model()
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    recipe_name = recipe_namer.predict(Image.open('./temp_image.jpg'))
+    return ' '.join(recipe_name)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ recipe.py
@@ -0,0 +1,166 @@
+import tensorflow as tf
+import numpy as np
+import pickle
+
+max_length = 22
+BATCH_SIZE = 64
+BUFFER_SIZE = 10000
+embedding_dim = 256
+units = 512
+vocab_size = 6556
+num_steps = 345
+# shape of the vector extracted from InceptionV3 is (64, 2048)
+# these two variables represent that
+features_shape = 2048
+attention_features_shape = 64
+
+# Model architecture from google's image_caption colab example (link)
+
+
+class BahdanauAttention(tf.keras.Model):
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, features, hidden):
+        # features(CNN_encoder output) shape == (batch_size, 64, embedding_dim)
+
+        # hidden shape == (batch_size, hidden_size)
+        # hidden_with_time_axis shape == (batch_size, 1, hidden_size)
+        hidden_with_time_axis = tf.expand_dims(hidden, 1)
+
+        # score shape == (batch_size, 64, hidden_size)
+        score = tf.nn.tanh(self.W1(features) + self.W2(hidden_with_time_axis))
+
+        # attention_weights shape == (batch_size, 64, 1)
+        # we get 1 at the last axis because we are applying score to self.V
+        attention_weights = tf.nn.softmax(self.V(score), axis=1)
+
+        # context_vector shape after sum == (batch_size, hidden_size)
+        context_vector = attention_weights * features
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        return context_vector, attention_weights
+
+
+class CNN_Encoder(tf.keras.Model):
+        # Since we have already extracted the features and dumped it using pickle
+        # This encoder passes those features through a Fully connected layer
+    def __init__(self, embedding_dim):
+        super(CNN_Encoder, self).__init__()
+        # shape after fc == (batch_size, 64, embedding_dim)
+        self.fc = tf.keras.layers.Dense(embedding_dim)
+
+    def call(self, x):
+        x = self.fc(x)
+        x = tf.nn.relu(x)
+        return x
+
+
+class RNN_Decoder(tf.keras.Model):
+    def __init__(self, embedding_dim, units, vocab_size):
+        super(RNN_Decoder, self).__init__()
+        self.units = units
+
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(self.units,
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
+        self.fc1 = tf.keras.layers.Dense(self.units)
+        self.fc2 = tf.keras.layers.Dense(vocab_size)
+
+        self.attention = BahdanauAttention(self.units)
+
+    def call(self, x, features, hidden):
+        # defining attention as a separate model
+        context_vector, attention_weights = self.attention(features, hidden)
+
+        # x shape after passing through embedding == (batch_size, 1, embedding_dim)
+        x = self.embedding(x)
+
+        # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+
+        # passing the concatenated vector to the GRU
+        output, state = self.gru(x)
+
+        # shape == (batch_size, max_length, hidden_size)
+        x = self.fc1(output)
+
+        # x shape == (batch_size * max_length, hidden_size)
+        x = tf.reshape(x, (-1, x.shape[2]))
+
+        # output shape == (batch_size * max_length, vocab)
+        x = self.fc2(x)
+
+        return x, state, attention_weights
+
+    def reset_state(self, batch_size):
+        return tf.zeros((batch_size, self.units))
+
+
+class RecipeNamer():
+    def __init__(self, image_features_extract_model, encoder, decoder, tokenizer):
+        self.image_features_extract_model = image_features_extract_model
+        self.encoder = encoder
+        self.decoder = decoder
+        self.tokenizer = tokenizer
+
+    def process_image(self, img):
+        img = tf.image.resize(img, (299, 299))
+        img = tf.keras.applications.inception_v3.preprocess_input(img)
+        return img
+
+    def predict(self, image):
+        hidden = self.decoder.reset_state(batch_size=1)
+
+        temp_input = tf.expand_dims(self.process_image(np.array(image)), 0)
+        img_tensor_val = self.image_features_extract_model(temp_input)
+        img_tensor_val = tf.reshape(
+            img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+
+        features = self.encoder(img_tensor_val)
+
+        dec_input = tf.expand_dims([self.tokenizer.word_index['<start>']], 0)
+        result = []
+
+        for i in range(max_length):
+            predictions, hidden, attention_weights = self.decoder(
+                dec_input, features, hidden)
+
+            predicted_id = tf.argmax(predictions[0]).numpy()
+            result.append(self.tokenizer.index_word[predicted_id])
+
+            if self.tokenizer.index_word[predicted_id] == '<end>':
+                return result
+
+            dec_input = tf.expand_dims([predicted_id], 0)
+
+        return result
+
+
+def create_models():
+    image_model = tf.keras.applications.InceptionV3(include_top=False,
+                                                    weights='imagenet')
+    new_input = image_model.input
+    hidden_layer = image_model.layers[-1].output
+
+    image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
+
+    encoder = CNN_Encoder(embedding_dim)
+    decoder = RNN_Decoder(embedding_dim, units, vocab_size)
+
+    encoder.load_weights('./model/recipe_encoder_weights')
+    decoder.load_weights('./model/recipe_decoder_weights')
+    with open('./model/tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    return image_features_extract_model, encoder, decoder, tokenizer
+
+
+def get_model():
+    image_features_extract_model, encoder, decoder, tokenizer = create_models()
+    return RecipeNamer(image_features_extract_model, encoder, decoder, tokenizer)
